528c59150f11ba71f4d12245ec2a0e718258d55a
alionapermes <alionapermes@gmail.com>
2024-02-26 02:55:27 +0700

    add experimental components

---
+++ apps/webapp/src/components/Charsheet.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import GeneralInfo from './blocks/GeneralInfo.vue'
+
+const props = defineProps<{ hpPerLvl: number }>()
+
+const lvl = ref(1)
+</script>
+
+<template>
+  <div>
+    <h1>Pathfinder 2e</h1>
+  </div>
+  <GeneralInfo
+    charName="abobus"
+    playerName="bibunus"
+    ancestry="gnome"
+    background="acrobat"
+    klass="ranger"
+  />
+  <div>
+    <div>
+      <h3>Total HP: {{ lvl * props.hpPerLvl }}</h3>
+    </div>
+    <div>
+      <button type="button" class="btn btn-blue" @click="lvl--">Lvl -</button>
+      <button type="button" class="btn btn-blue" @click="lvl++">Lvl +</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .btn {
+    @apply font-bold py-2 px-4 rounded;
+  }
+  .btn-blue {
+    @apply bg-blue-500 text-white;
+  }
+  .btn-blue:hover {
+    @apply bg-blue-700;
+  }
+</style>
---
+++ apps/webapp/src/components/blocks/GeneralInfo.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  const props = defineProps<{
+    charName: string,
+    playerName: string,
+    ancestry: string,
+    background: string,
+    klass: string,
+  }>()
+</script>
+
+<template>
+  <div>
+    <p>Character name: {{ props.charName }}</p>
+    <p>Player name: {{ props.playerName }}</p>
+    <p>Ancestry: {{ props.ancestry }}</p>
+    <p>Background: {{ props.background }}</p>
+    <p>Class: {{ props.klass }}</p>
+  </div>
+</template>
